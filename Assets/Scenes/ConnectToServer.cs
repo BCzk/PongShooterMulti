b365aaab412d06d03ccd6157b243a65bfcc49a61
SantinoBaldan <santino.bcs@gmail.com>
2024-08-22 20:00:30 -0300

    Photon + Connect & Menu Scripts

---
+++ Assets/Scenes/ConnectToServer.cs
@@ -0,0 +1,18 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ConnectToServer : MonoBehaviourPunCallbacks
+{
+    private void Awake()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
