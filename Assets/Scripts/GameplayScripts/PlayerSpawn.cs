fa96b9b42855ec8f78c59d63129a3f3cb75be531
ThiagonRubio <65839783+ThiagonRubio@users.noreply.github.com>
2024-08-29 09:54:54 -0300

    Spawn distinto para c/jugador

--- Assets/Scripts/GameplayScripts/PlayerSpawn.cs
+++ Assets/Scripts/GameplayScripts/PlayerSpawn.cs
@@ -7,7 +7,8 @@ public class PlayerSpawn : MonoBehaviour
 {
     [SerializeField] private List<Transform> startingPositions;
     [SerializeField] private List<GameObject> playerPrefabs;
-    private GameObject player;
+    
+    private GameObject _player;
     private PhotonView pv;
 
     private static int _playersSpawned; //Esto aún no funciona
@@ -20,25 +21,35 @@ public class PlayerSpawn : MonoBehaviour
 
     private void Start()
     {
-        player = PhotonNetwork.Instantiate(playerPrefabs[_playersSpawned - 1].name, startingPositions[_playersSpawned-1].position, Quaternion.identity);
+        //_player = PhotonNetwork.Instantiate(playerPrefabs[_playersSpawned - 1].name, startingPositions[_playersSpawned-1].position, Quaternion.identity);
+
+        switch (PhotonNetwork.CurrentRoom.PlayerCount)
+        {
+            case 1:
+                PhotonNetwork.Instantiate(playerPrefabs[0].name, startingPositions[0].position, Quaternion.identity);
+                break;
+            case 2:
+                PhotonNetwork.Instantiate(playerPrefabs[1].name, startingPositions[1].position, Quaternion.identity);
+                break;
+        }
 
         //pv.RPC("SetColorToPlayers", RpcTarget.OthersBuffered);
     }
 
-    [PunRPC]
-    private void SetColorToPlayers()
-    {
-        if (pv.IsMine)
-        {
-            switch (PhotonNetwork.CurrentRoom.PlayerCount)
-            {
-                case 1:
-                    player.GetComponent<SpriteRenderer>().color = Color.red;
-                    break;
-                case 2:
-                    player.GetComponent<SpriteRenderer>().color = Color.blue;
-                    break;
-            }
-        }
-    }
+    // [PunRPC]
+    // private void SetColorToPlayers()
+    // {
+    //     if (pv.IsMine)
+    //     {
+    //         switch (PhotonNetwork.CurrentRoom.PlayerCount)
+    //         {
+    //             case 1:
+    //                 player.GetComponent<SpriteRenderer>().color = Color.red;
+    //                 break;
+    //             case 2:
+    //                 player.GetComponent<SpriteRenderer>().color = Color.blue;
+    //                 break;
+    //         }
+    //     }
+    // }
 }
