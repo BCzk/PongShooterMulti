2fa457b74affd09b9301dbf49dc3aa5de2945db4
SantinoBaldan <santino.bcs@gmail.com>
2024-09-02 15:05:48 -0300

    Bullet Photon Fix

--- Assets/Scripts/GameplayScripts/Bullet/BulletController.cs
+++ Assets/Scripts/GameplayScripts/Bullet/BulletController.cs
@@ -8,13 +8,13 @@ public class BulletController : MonoBehaviour
 {
     private BulletModel _bulletModel;
     PhotonView pv;
-    
+
     void Start()
     {
         pv = GetComponent<PhotonView>();
         _bulletModel = GetComponent<BulletModel>();
     }
-    
+
     void Update()
     {
         _bulletModel.Move();
@@ -28,19 +28,34 @@ public class BulletController : MonoBehaviour
         // }
     }
 
-    private void OnBecameInvisible() //Por si se sale de la pantalla
+    private void OnBecameInvisible() // Por si el objeto se sale de la pantalla
     {
-        if (!PhotonNetwork.IsMasterClient)
+        if (PhotonNetwork.IsMasterClient)
         {
+            if (!pv.IsMine)
+            {
+                Debug.Log("Intentando transferir propiedad al Master Client para el objeto " + gameObject, gameObject);
+                pv.TransferOwnership(PhotonNetwork.MasterClient);
 
-            pv.RPC("RequestDestroy", RpcTarget.MasterClient, pv.ViewID);
+                StartCoroutine(WaitUntilOwnershipAndDestroy());
+            }
+            else
+            {
+                Debug.Log("El objeto ya es del Master Client, destruyéndolo " + gameObject, gameObject);
+                PhotonNetwork.Destroy(pv);
+            }
         }
-        
     }
 
-    [PunRPC]
-    private void RequestDestroy(int id)
+    private IEnumerator WaitUntilOwnershipAndDestroy()
     {
-        PhotonNetwork.Destroy(PhotonView.Find(id).gameObject);
+        while (!pv.IsMine)
+        {
+            yield return null;
+        }
+
+        Debug.Log("Propiedad transferida exitosamente al Master Client. Destruyendo " + gameObject, gameObject);
+        PhotonNetwork.Destroy(pv);
     }
+
 }
