5b57d0628662f7dd4b065aa2fbd8a38b1866a6e4
SantinoBaldan <santino.bcs@gmail.com>
2024-09-02 17:12:24 -0300

    BallBounce

--- Assets/Scripts/GameplayScripts/Ball/BallController.cs
+++ Assets/Scripts/GameplayScripts/Ball/BallController.cs
@@ -6,6 +6,7 @@ public class BallController : MonoBehaviour
 {
     private Rigidbody2D _rb;
     private BallModel _model;
+    private Vector2 _lastVelocity;
 
     private void Awake()
     {
@@ -15,23 +16,45 @@ public class BallController : MonoBehaviour
 
     private void Start()
     {
-        int randomDir = Random.Range(0, 2);
-        if (randomDir == 0)
-        {
-            AddForceToBall(new Vector2(0.2f, 1));
-        }
-        else
-        {
-            AddForceToBall(new Vector2(-0.2f, -1));
-        }
+        //int randomDir = Random.Range(0, 2);
+        //if (randomDir == 0)
+        //{
+        //    AddForceToBall(new Vector2(0.2f, 1));
+        //}
+        //else
+        //{
+        //    AddForceToBall(new Vector2(-0.2f, -1));
+        //}
 
 
         //WIP
         //_rb.velocity = Vector2.one * _model.Speed;   //_model.ChooseRandomDirection();
+
+        _rb.velocity = Random.insideUnitCircle.normalized * _model.Speed;
+    }
+    private void Update()
+    {
+        _lastVelocity = _rb.velocity;
     }
 
-    private void AddForceToBall(Vector2 dir)
+    //private void AddForceToBall(Vector2 dir)
+    //{
+    //    _rb.AddForce(_model.Speed * Time.deltaTime * dir);
+    //}
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        _rb.AddForce(_model.Speed * Time.deltaTime * dir);
+        var aux = 0.2f;
+
+        Vector2 newDirection = Vector2.Reflect(_lastVelocity.normalized, collision.contacts[0].normal);
+
+        Vector2 randomFactor = Random.insideUnitCircle * aux;
+        newDirection += randomFactor;
+        newDirection.Normalize();
+
+        _rb.velocity = newDirection * _model.Speed;
+
+        //Vector2 randomDirection = Random.insideUnitCircle.normalized;
+
+        //_rb.velocity = randomDirection * _model.Speed;
     }
 }
--- Assets/Scripts/GameplayScripts/Ball/BallModel.cs
+++ Assets/Scripts/GameplayScripts/Ball/BallModel.cs
@@ -7,18 +7,18 @@ public class BallModel : MonoBehaviour
 {
     public float Speed => speed;
     
-    private Vector2 _auxDirectionVector;
+    //private Vector2 _auxDirectionVector;
     [SerializeField] private float speed;
     
-    public Vector2 ChooseRandomDirection()
-    {
-        _auxDirectionVector.x = Random.Range(1, 360);
-        _auxDirectionVector.y = Random.Range(1, 360);
-        return _auxDirectionVector.normalized;
-    }
+    //public Vector2 ChooseRandomDirection()
+    //{
+    //    _auxDirectionVector.x = Random.Range(1, 360);
+    //    _auxDirectionVector.y = Random.Range(1, 360);
+    //    return _auxDirectionVector.normalized;
+    //}
 
-    public void ChangeBallColor()
-    {
+    //public void ChangeBallColor()
+    //{
         
-    }
+    //}
 }
